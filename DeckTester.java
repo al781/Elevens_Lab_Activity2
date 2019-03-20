30262626d1a7b1e28f7e39f5cc936172697fecd5
alim509 <al5091@pvsd.org>
2019-03-20 11:22:09 -0400

    Initial committ

---
+++ DeckTester.java
@@ -0,0 +1,35 @@
+public class DeckTester {
+
+	public static void main(String[] args) {
+		String[] rank1 = ["S", "L", "B"];
+		String[] rank2 = ["A", "M", "N"];
+		String[] rank3 = ["W", "R", "E"];
+
+		String[] suit1 = ["Shark", "Human", "Tiger"];
+		String[] suit2 = ["Cat", "Dog", "Pirhanna"];
+		String[] suit3 = ["Panda", "Lizard", "Turkey"];
+
+		int[] value1 = [5, 2, 7];
+		int[] value2 = [3, 4, 1];
+		int[] value3 = [8, 9, 6];
+
+		Deck one = new Card(rank1, suit1, value1);
+		Deck two = new Card(rank2, suit2, value2);
+		Deck three = new Card(rank3, suit3, value3);
+
+		for(int i = 0; i < one.size(); i++) {
+			one.deal();
+			one.size()
+		}
+
+		for(int j = 0; j < two.size(); j++) {
+			two.deal();
+			two.size()
+		}
+
+		for(int m = 0; m < three.size(); m++) {
+			three.deal();
+			three.size()
+		}
+	}
+}
